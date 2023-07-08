86dded310e5075550c06ed8b92e7e24b39db8485
OSIIOSdev <OSIIOS.dev@gmail.com>
2023-07-08 13:06:39 -0700

    Initial Commit

---
+++ ShowGreeting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShowGreeting
+//
+//  Created by OSIIOSdev on 08.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
